60ddbacf0d32433af17e9382e67a5e811f37612f
khannaprateek <pkhanna9859@gmail.com>
2018-08-17 14:20:33 +0530

    this is my second assignment

---
+++ assignment2.py
@@ -0,0 +1,40 @@
+#q1
+print("This will be displayed on the screen")
+
+#q2
+a='I am learning '
+b='python'
+print(a+b)
+
+#q3
+
+x,y,z=input().split()
+print(x)
+print(y)
+print(z)
+
+
+#q4
+print('''"Let's get started"''')
+
+#q5
+s='Acadview'
+course='Python'
+fees=5000
+print("s=%s"%(s))
+print("course=%s"%(course))
+print("fees=%d"%(fees))
+
+
+#q6
+name="Tony Stark"
+salary=1000000
+print(('%s"%d')%(name,salary))
+
+#q7
+radius= int((input()))
+area=3.14*radius**2
+print(area)
+
+
+
