b5fcc5610a04d2c95e603ebbea8c0511856ce78a
khannaprateek <37430092+khannaprateek@users.noreply.github.com>
2018-08-23 12:27:13 +0530

    Add files via upload

---
+++ assignmen6.py
@@ -0,0 +1,47 @@
+#q1
+
+def areaOfSphere(radius):
+    area=4/3 * 3.14 * (radius**2)
+    return area
+i=int(input("Enter the radius of the sphere"))
+area=areaOfSphere(i)
+print(area)
+
+#q2
+
+def perfect(number):
+      sum=0
+      for i in range(1,number):
+            if number%i is 0:
+                  sum=sum+i
+
+      if sum == number:
+            return number
+      else:
+            return 0
+
+
+for i in range(1,1001):
+      n=perfect(i)
+      if n is not 0:
+            print(n)
+
+
+#q3
+
+n=int(input("enter a number"))
+for i in range(1,11):
+      print(i*n)
+
+#q4
+
+def power(n,m):
+    if m is 1:
+        return n
+    else :
+      a= power(n,m-1)
+      return (n*a)
+n=int(input("enter a number"))
+m=int(input("enter a number"))
+number=power(n,m)
+print("n^m=",number)
